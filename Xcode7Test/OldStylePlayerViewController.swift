1a0cbe1fa2017d15007d391d3a7327ee7cb6407d
toshi0383 <t.suzuki326@gmail.com>
2015-06-13 19:52:16 +0900

    split class into 2classes to support older OS version

--- Xcode7Test/OldStylePlayerViewController.swift
+++ Xcode7Test/OldStylePlayerViewController.swift
@@ -12,28 +12,23 @@ import AVFoundation
 let url1 = NSURL(string: "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8")!
 let url2 = NSBundle.mainBundle().URLForResource("503_sd_mastering_modern_media_playback", withExtension: "mov")!
 
-@available(iOS 9.0, *)
-class OldStylePlayerViewController: UIViewController,AVPictureInPictureControllerDelegate {
+protocol OldStylePlayerViewControllerProtocol :AVPictureInPictureControllerDelegate {
+  func playInOldStyle()
+}
+class OldStylePlayerViewController: UIViewController, OldStylePlayerViewControllerProtocol {
   private var playerView:PlayerView!
-  private var pipController:AVPictureInPictureController?
   override func viewDidLoad() {
     super.viewDidLoad()
     navigationController?.navigationBarHidden = true
     playInOldStyle()
   }
-  private func playInOldStyle() {
+  func playInOldStyle() {
     playerView = PlayerView(frame: view.frame)
     playerView.player = AVPlayer(URL: url2)
     view.addSubview(playerView)
     playerView.player.play()
-
-    if AVPictureInPictureController.isPictureInPictureSupported() {
-      pipController = AVPictureInPictureController(playerLayer: playerView.layer as! AVPlayerLayer)
-      pipController?.delegate = self
-    }
   }
   override func viewDidAppear(animated: Bool) {
-//    pipController?.startPictureInPicture()
   }
   private class PlayerView :UIView {
     override class func layerClass() -> AnyClass {
@@ -50,11 +45,30 @@ class OldStylePlayerViewController: UIViewController,AVPictureInPictureControlle
       }
     }
   }
+  //
+  // alternative customization is expected..
+  //
+}
+@available(iOS 9.0, *)
+class PiPOldStylePlayerViewController : OldStylePlayerViewController {
+  private var pipController:AVPictureInPictureController?
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    if AVPictureInPictureController.isPictureInPictureSupported() {
+      pipController = AVPictureInPictureController(playerLayer: playerView.layer as! AVPlayerLayer)
+      pipController?.delegate = self
+      //    pipController?.startPictureInPicture()
+    }
+  }
+
   func pictureInPictureController(pictureInPictureController: AVPictureInPictureController, restoreUserInterfaceForPictureInPictureStopWithCompletionHandler completionHandler: (Bool) -> Void) {
     completionHandler(true)
   }
   func pictureInPictureControllerWillStartPictureInPicture(pictureInPictureController: AVPictureInPictureController) {
     print("PiP start !")
   }
+
+
 }
 
